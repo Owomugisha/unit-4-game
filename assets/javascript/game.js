7f8c2845f37b39d3f0043749b159bdfb6979cd7e
Owomugisha <brinnetbirungi@yahoo.com>
2018-12-02 19:26:59 -0500

    add document. ready, created a function for random number, created a function that will reset and start the game, in which i created random for each crystal and added them as attrs. created event listener and logic for win and losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,62 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // min and max included
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    //setting var to zero
 
-//setting var to zero
-var Total = 0;
-var wins = 0;
-var losses = 0;
+    var score;
+    var wins = 0;
+    var losses = 0;
+    // 1. generante random number 19 and 120
+    var randomNumber;
+    //create a function that will be called to reset the game 
+    function gameStart() {
+        score = 0;
+        randomNumber = randomIntFromInterval(19, 120);
+        $(".randomNumber").text(randomNumber); // display randon number
+        //2. each crystal should have a random hidden between 1-12
+        //create 4 vars for the each crystal
+        var whiteCrystal = randomIntFromInterval(1, 12);
+        var greenCrystal = randomIntFromInterval(1, 12);
+        var blueCrystal = randomIntFromInterval(1, 12);
+        var orangeCrystal = randomIntFromInterval(1, 12);
+        //console.log(whiteCrystal, greenCrystal, blueCrystal, orangeCrystal);
+        //adding attr to each crystal
+        $("#diamond1").attr("crystalValue", whiteCrystal);
+        $("#green").attr("crystalValue", greenCrystal);
+        $("#blue").attr("crystalValue", blueCrystal);
+        $("#orange").attr("crystalValue", orangeCrystal);
+        $(".score").text(0);
+    }
 
+    //console.log(randomNumber);
+    gameStart();
+    //create .on click on each crystal or add event listener 
+    $(".crystal").click(function () {
+        //grabbing crystal value and changing it to a number
+        var crystaValue = parseInt($(this).attr("crystalValue"));
+        //updating the score value of each clicked crystal
+        score = score + crystaValue;
+        //displaying the updated score
+        $(".score").text(score);
+        console.log(crystaValue)
+        //if the value of score is equal to randomNumber increment win by 1
+        if (score == randomNumber) { //updating the win value 
+            wins++;
+            $("#wins").text(wins);
+            $("#winAndLoss").html('<p>You win ☺</p>'); //adding html unicode to the win
+            gameStart();
+            //if the value of score is greater than randomNumber increment losses by 1
+        } else if (score > randomNumber) { //updating the losses value
+            losses++;
+            $("#losses").text(losses);
+            $("#winAndLoss").html('<p>You lose &#9785;</p>'); //adding html unicode to the losses
 
+            gameStart();
+        }
+        //re
+
+    })
 
 });
